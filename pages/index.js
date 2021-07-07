988f71607ce5921f504f4d50081142768f404dd8
Francisco Mazzoni <fran.mazzoni@gmail.com>
2021-07-07 16:00:56 -0300

    group api mock

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch("https://tryfamly.co/api/daycare/tablet/group?accessToken=234ffdb8-0889-4be3-b096-97ab1679752c&groupId=11fc220c-ebba-4e55-9346-cd1eed714620&institutionId=fb6c8114-387e-4051-8cf7-4e388a77b673")
+  const res = await fetch("https://mockend.com/pucho/hire-me/groups")
   const data = await res.json()
 
   if (!data) {
